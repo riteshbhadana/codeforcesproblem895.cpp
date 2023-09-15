e4a7a6247cab658c9ce7102ac62b9c26bcd48877
Ritesh <142724392+riteshbhadana@users.noreply.github.com>
2023-09-15 06:45:57 +0530

    Update codeforcesproblem895.cpp

--- codeforcesproblem895.cpp
+++ codeforcesproblem895.cpp
@@ -15,3 +15,31 @@ int main()
         cout << ans << endl;
     }
 }
+
+
+
+problem b
+The  corridor or there and back again 
+
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long int ll;
+int mod = 1000000007;
+int main() 
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t = 1;
+    cin >> t;
+    while(t--)
+    {
+        ll n;
+        cin >> n;
+        ll ans = 1e18;
+        for(int i = 1; i <= n; i++) {
+            ll d, s; cin >> d >> s;
+            ans = min(ans, (2 * d + s - 1) / 2);
+        }
+        cout << ans << '\n';
+    }
+}
